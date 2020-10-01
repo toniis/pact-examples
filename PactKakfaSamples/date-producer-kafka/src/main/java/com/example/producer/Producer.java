a090804afd8ac464eba70e1a42e85d264475ea1a
Toni (Sofist) <otoniel.isidoro@sofist.com.br>
2020-09-30 21:04:43 -0300

    [feat] add kafka samples

---
+++ PactKakfaSamples/date-producer-kafka/src/main/java/com/example/producer/Producer.java
@@ -0,0 +1,27 @@
+package com.example.producer;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import java.time.LocalDate;
+
+public class Producer {
+
+    @Autowired
+    private KafkaTemplate<String, ProducerDateInfo> kafkaTemplate;
+
+    @Value(value = "${kafka.topic}")
+    private String topicName;
+
+    public void send() {
+        LocalDate localDate = LocalDate.now();
+        kafkaTemplate.send(topicName, generateMessage(localDate));
+    }
+
+    @NotNull
+    public ProducerDateInfo generateMessage(LocalDate localDate) {
+        return new ProducerDateInfo(localDate.toString(), localDate.isLeapYear());
+    }
+}
---
+++ PactKakfaSamples/date-producer-kafka/src/test/java/com/example/producer/pact/DateProducerTest.java
@@ -0,0 +1,31 @@
+package com.example.producer.pact;
+
+import au.com.dius.pact.provider.PactVerifyProvider;
+import au.com.dius.pact.provider.junit.PactRunner;
+import au.com.dius.pact.provider.junit5.MessageTestTarget;
+import au.com.dius.pact.provider.junitsupport.Consumer;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+import au.com.dius.pact.provider.junitsupport.target.TestTarget;
+import com.example.producer.Producer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.junit.runner.RunWith;
+
+import java.time.LocalDate;
+
+@RunWith(PactRunner.class)
+@Provider("dateProviderKafka")
+@Consumer("dateConsumerKafka")
+@PactBroker(host = "localhost", port = "9292")
+public class DateProducerTest {
+
+    @TestTarget
+    public final MessageTestTarget target = new MessageTestTarget();
+
+    @SneakyThrows
+    @PactVerifyProvider("valid date from kafka provider")
+    public String verifyDateInformationMessage() {
+        return new ObjectMapper().writeValueAsString(new Producer().generateMessage(LocalDate.now()));
+    }
+}
